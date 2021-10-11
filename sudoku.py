3d96beb58c2e06480dc9733961e3c8453efa3f00
Jelly_Pudding <samwalters11111@hotmail.com>
2021-10-11 18:40:12 +0100

    add vertical checker

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Sudoku:
 	def __init__(self):
 		self.sudoku_board = [[[0 for item_in_box_row in range(3)] for box_per_box_row in range(3)] for horizontal_line in range(9)]
@@ -22,22 +24,28 @@ class Sudoku:
 				summed = sum(count_numbers)
 				if summed > 1:
 					return False
-
+		#Checks vertical
+		two_d_list = []
+		for e1 in suk.sudoku_board:
+			for e2 in e1:
+				two_d_list.append(e2)
+		one_d_list = []
+		for e1 in two_d_list:
+			for e2 in e1:
+                		one_d_list.append(e2)
+		for i in range(9):
+			spliced_list = one_d_list[i:]
+			every_other_element = spliced_list[::9]
+			for numbers in range(1, 10, 1):	
+				counted = every_other_element.count(numbers)
+				if counted >1:
+					return False
 
 suk = Sudoku()
 
-
-suk.sudoku_board[0][1][1] = 1
-suk.sudoku_board[5][1][1] = 5
-suk.sudoku_board[7][1][1] = 5
-suk.sudoku_board[6][1][1] = 5
-suk.sudoku_board[8][1][1] = 5
-suk.sudoku_board[2][1][1] = 5
-suk.sudoku_board[3][1][1] = 5
-suk.sudoku_board[1][1][1] = 5
-suk.sudoku_board[0][2][2] = 1
-
-
+suk.sudoku_board[8][2][2] = 1
+suk.sudoku_board[7][2][2] = 1
 suk.printer()
+
 
 print(suk.checker())
