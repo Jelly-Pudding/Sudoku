f6e88ed4dc6325562eb45a12744a79e2716c1595
Jelly_Pudding <samwalters11111@hotmail.com>
2021-10-11 21:52:13 +0100

    now checks the boxes as well

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 class Sudoku:
 	def __init__(self):
 		self.sudoku_board = [[[0 for item_in_box_rows in range(3)] for box_per_row_of_boxes in range(3)] for horizontal_line in range(9)]
@@ -17,7 +15,8 @@ class Sudoku:
 				print("=========================")									
 
 	def checker(self):
-
+		skip_list = False
+		number = 0
 		#conversion to 2d list then 1d list for easier checking
 
 		two_d_list = []
@@ -48,145 +47,39 @@ class Sudoku:
 				if counted >1:
 					return False
 
+		for i in range(0, 28, 3):
+			if i == 9:
+				skip_list = True
+				number += 15
+			elif i == 18:
+				number += 18
+				sliced_list = one_d_list[33:33+27]
+			elif skip_list == True:
+				sliced_list = one_d_list[i+number:i+number+27]
+			else:
+				sliced_list = one_d_list[i:i+27]
+			first_three = sliced_list[0:3]
+			second_three = sliced_list[9:12]
+			third_three = sliced_list[18:21]
+			all_elements = first_three + second_three + third_three
+			for numbers in range(1, 10, 1):
+				counted = all_elements.count(numbers)
+				if counted >1:
+					return False
+
+
 suk = Sudoku()
 
-suk.sudoku_board[0][0][0] = 1
-suk.sudoku_board[0][0][1] = 1
-suk.sudoku_board[0][0][2] = 1
-suk.sudoku_board[1][0][0] = 2
-suk.sudoku_board[1][0][1] = 2
-suk.sudoku_board[1][0][2] = 2
-suk.sudoku_board[2][0][0] = 3
-suk.sudoku_board[2][0][1] = 3
-suk.sudoku_board[2][0][2] = 3
-
-suk.sudoku_board[0][1][0] = 4
-suk.sudoku_board[0][1][1] = 4
-suk.sudoku_board[0][1][2] = 4
-suk.sudoku_board[1][1][0] = 5
-suk.sudoku_board[1][1][1] = 5
-suk.sudoku_board[1][1][2] = 5
-suk.sudoku_board[2][1][0] = 6
-suk.sudoku_board[2][1][1] = 6
-suk.sudoku_board[2][1][2] = 6
-
-suk.sudoku_board[0][2][0] = 7
-suk.sudoku_board[0][2][1] = 7
-suk.sudoku_board[0][2][2] = 7
-suk.sudoku_board[1][2][0] = 8
-suk.sudoku_board[1][2][1] = 8
-suk.sudoku_board[1][2][2] = 8
-suk.sudoku_board[2][2][0] = 9
-suk.sudoku_board[2][2][1] = 9
-suk.sudoku_board[2][2][2] = 9
-
-suk.sudoku_board[3][1][0] = 1
-suk.sudoku_board[3][1][1] = 1
-suk.sudoku_board[3][1][2] = 1
-suk.sudoku_board[4][1][0] = 2
-suk.sudoku_board[4][1][1] = 2
-suk.sudoku_board[4][1][2] = 2
-suk.sudoku_board[5][1][0] = 3
-suk.sudoku_board[5][1][1] = 3
-suk.sudoku_board[5][1][2] = 3
-
-suk.sudoku_board[3][2][0] = 4
-suk.sudoku_board[3][2][1] = 4
-suk.sudoku_board[3][2][2] = 4
-suk.sudoku_board[4][2][0] = 5
-suk.sudoku_board[4][2][1] = 5
-suk.sudoku_board[4][2][2] = 5
-suk.sudoku_board[5][2][0] = 6
-suk.sudoku_board[5][2][1] = 6
-suk.sudoku_board[5][2][2] = 6
-
-suk.sudoku_board[3][0][0] = 7
-suk.sudoku_board[3][0][1] = 7
-suk.sudoku_board[3][0][2] = 7
-suk.sudoku_board[4][0][0] = 8
-suk.sudoku_board[4][0][1] = 8
-suk.sudoku_board[4][0][2] = 8
-suk.sudoku_board[5][0][0] = 9
-suk.sudoku_board[5][0][1] = 9
-suk.sudoku_board[5][0][2] = 9
-
-suk.sudoku_board[6][1][0] = 4
-suk.sudoku_board[6][1][1] = 4
-suk.sudoku_board[6][1][2] = 4
-suk.sudoku_board[7][1][0] = 5
-suk.sudoku_board[7][1][1] = 5
-suk.sudoku_board[7][1][2] = 5
-suk.sudoku_board[8][1][0] = 6
-suk.sudoku_board[8][1][1] = 6
-suk.sudoku_board[8][1][2] = 6
-
-suk.sudoku_board[6][0][0] = 1
-suk.sudoku_board[6][0][1] = 1
-suk.sudoku_board[6][0][2] = 1
-suk.sudoku_board[7][0][0] = 2
-suk.sudoku_board[7][0][1] = 2
-suk.sudoku_board[7][0][2] = 2
-suk.sudoku_board[8][0][0] = 3
-suk.sudoku_board[8][0][1] = 3
-suk.sudoku_board[8][0][2] = 3
-
-suk.sudoku_board[6][2][0] = 7
-suk.sudoku_board[6][2][1] = 7
-suk.sudoku_board[6][2][2] = 7
-suk.sudoku_board[7][2][0] = 8
-suk.sudoku_board[7][2][1] = 8
-suk.sudoku_board[7][2][2] = 8
-suk.sudoku_board[8][2][0] = 9
-suk.sudoku_board[8][2][1] = 9
-suk.sudoku_board[8][2][2] = 9
-
+suk.sudoku_board[0][0][0] = 9
+suk.sudoku_board[1][1][1] = 9
+suk.sudoku_board[0][2][2] = 5
+suk.sudoku_board[1][2][0] = 4
+suk.sudoku_board[3][1][2] = 6
+suk.sudoku_board[4][1][0] = 7
+suk.sudoku_board[8][2][2] = 2
+suk.sudoku_board[7][2][0] = 1
 
 
 suk.printer()
 
-two_d_list = []
-for e1 in suk.sudoku_board:
-	for e2 in e1:
-		two_d_list.append(e2)
-one_d_list = []
-for e1 in two_d_list:
-	for e2 in e1:
-		one_d_list.append(e2)
-
-
-
-print(one_d_list)
-
-number = 0
-skip_list = False
-last_boxes = False
-
-for i in range(0, 29, 3):
-	if i == 9:
-		skip_list = True
-		number += 15
-	elif i == 18:
-		number += 18
-		sliced_list = one_d_list[33:33+27]
-		print(sliced_list)
-	elif skip_list == True:
-		sliced_list = one_d_list[i+number:i+number+27]
-		print(sliced_list)
-	else:
-		sliced_list = one_d_list[i:i+27]
-		print(sliced_list)
-
-
-
-
-
-
-the_elements = one_d_list[0:3]
-further_elements = one_d_list[9:12]
-more_elements = one_d_list[18:21]
-
-
-
-print(the_elements)
-print(further_elements)
-print(more_elements)
+print(suk.checker())
