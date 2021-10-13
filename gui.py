8e41b1725a1e4dc385b09fc3136f09b2df404c1a
Jelly_Pudding <samwalters11111@hotmail.com>
2021-10-13 20:35:54 +0100

    add gui file

---
+++ gui.py
@@ -0,0 +1,61 @@
+import pygame, sys
+from sudoku import Sudoku
+from sudoku import backtracking
+
+bot = Sudoku()
+
+bot.sudoku_board[1][0][0] = 5
+bot.sudoku_board[2][0][0] = 3
+bot.sudoku_board[4][0][0] = 8
+bot.sudoku_board[7][0][0] = 2
+bot.sudoku_board[2][0][1] = 8
+bot.sudoku_board[4][0][1] = 4
+bot.sudoku_board[7][0][1] = 7
+bot.sudoku_board[3][0][2] = 6
+bot.sudoku_board[5][0][2] = 5
+bot.sudoku_board[6][0][2] = 4
+bot.sudoku_board[2][1][0] = 7
+bot.sudoku_board[3][1][0] = 4
+bot.sudoku_board[7][1][0] = 3
+bot.sudoku_board[0][1][1] = 2
+bot.sudoku_board[4][1][2] = 3
+bot.sudoku_board[8][1][2] = 9
+bot.sudoku_board[0][2][0] = 6
+bot.sudoku_board[4][2][0] = 9
+bot.sudoku_board[8][2][1] = 1
+bot.sudoku_board[2][2][2] = 2
+bot.sudoku_board[5][2][2] = 8
+bot.sudoku_board[7][2][2] = 6
+
+#bot.printer()
+#backtracking(bot)
+
+class Pane(object):
+	def __init__(self):
+		pygame.init()
+		self.font = pygame.font.SysFont("Arial", 17)
+		pygame.display.set_caption("Sudoku")	
+		self.screen = pygame.display.set_mode((200, 200), 0, 32)
+		self.screen.fill((255, 255, 255))
+		pygame.display.update()
+		self.FPS = 60
+	def add_rectangle(self):
+		self.rect = pygame.draw.rect(self.screen, ((0, 0, 0)), (0, 0, 20, 20), 2)
+		pygame.display.update()
+	def add_text(self):
+		self.screen.blit(self.font.render("0", True, (255, 0, 0)), (6, 1))
+		pygame.display.update()
+
+def main():
+	pan = Pane()
+	pan.add_rectangle()
+	pan.add_text()
+	clock = pygame.time.Clock()
+	while True:
+		clock.tick(60)
+		for event in pygame.event.get():
+			if event.type==pygame.QUIT:
+				pygame.quit(); sys.exit();
+
+if __name__ == "__main__":
+	main()
